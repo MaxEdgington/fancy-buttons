8de17fe7c2e292ca701d69201c415a784667583e
Max Edgington <edgington.m.w@gmail.com>
2023-09-05 20:19:56 -0400

    add completed fancy buttons project in react

---
+++ src/components/AngryButton.js
@@ -0,0 +1,22 @@
+import React, {useReducer} from "react";
+
+function AngryButton(props) {
+
+   const [anger, dispatch] = useReducer((anger, action) => {
+       return anger > 1 ? 0 : anger + action;
+    }, 0)
+
+  
+  return (
+    <button
+      style={{ backgroundColor: `rgba(255,0,0,${anger})` }}
+      onClick={() => dispatch(0.1)}
+      className="AngryButton"
+    >
+      {anger < 1 && <span>Don't click me too much! </span>}
+      {anger > 1 && <span>Rawr!</span>}
+    </button>
+  );
+}
+
+export default AngryButton;
---
+++ src/components/TextRepeaterButton.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+function TextRepeaterButton() {
+  const [repetitions, setRepetitions] = useState(1);
+  const textArray = [];
+  const handleClick = () => {
+    setRepetitions(repetitions + 1);
+  };
+  for (let i = 0; i < repetitions; i++) {
+    textArray.push(<span key={i}>I like this text</span>);
+  }
+
+  return (
+    <button className="TextRepeaterButton" onClick={handleClick}>
+      {textArray}
+    </button>
+  );
+}
+
+export default TextRepeaterButton;
